74d66de1740caa6e3fad604e122a63689d690989
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2025-01-18 11:24:10 +0200

    wip: Parse bool

--- build.zig
+++ build.zig
@@ -10,12 +10,22 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const exe_unit_tests = b.addTest(.{
+    const tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_tests.step);
+
+    {
+        const tests_check = b.addTest(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const check_step = b.step("check", "Check if foo compiles");
+        check_step.dependOn(&tests_check.step);
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,8 @@
-// (c) 2024 Lawrence Laz
+// (c) 2025 Lawrence Laz
 // This code is licensed under MIT license (see LICENSE for details)
+
+// TOOD: Implement parsing as per https://www.json.org/json-en.html
+// TODO: Pass the test suite at https://github.com/nst/JSONTestSuite
 
 const std = @import("std");
 
@@ -22,14 +25,14 @@ pub const json = struct {
     fn stringifyValue(value: anytype, writer: std.io.AnyWriter) !void {
         const T = @TypeOf(value);
         switch (@typeInfo(T)) {
-            .Bool => try writer.writeAll(if (value) "true" else "false"),
-            .Int => try stringifyInt(value, writer),
-            .ComptimeInt => try writer.print("{d}", .{value}),
-            .Float => try writer.print("{d}", .{value}),
-            .ComptimeFloat => try writer.print("{d}", .{value}),
-            .Pointer => |ptr_info| switch (ptr_info.size) {
+            .bool => try writer.writeAll(if (value) "true" else "false"),
+            .int => try stringifyInt(value, writer),
+            .comptime_int => try writer.print("{d}", .{value}),
+            .float => try writer.print("{d}", .{value}),
+            .comptime_float => try writer.print("{d}", .{value}),
+            .pointer => |ptr_info| switch (ptr_info.size) {
                 .One => switch (@typeInfo(ptr_info.child)) {
-                    .Array => |array_info| if (array_info.child == u8)
+                    .array => |array_info| if (array_info.child == u8)
                         try writer.writeAll(@as([]const std.meta.Elem(ptr_info.child), value))
                     else
                         try stringifyArray(@as([]const std.meta.Elem(ptr_info.child), value), writer),
@@ -46,11 +49,11 @@ pub const json = struct {
                 },
                 else => @compileError("Cannot stringify type '" ++ @typeName(T) ++ "'"),
             },
-            .Array => |array_info| if (array_info.child == u8)
+            .array => |array_info| if (array_info.child == u8)
                 try writer.writeAll(@as([]const array_info.child, &value))
             else
                 try stringifyArray(@as([]const array_info.child, &value), writer),
-            .Struct => |struct_info| if (struct_info.is_tuple)
+            .@"struct" => |struct_info| if (struct_info.is_tuple)
                 try stringifyTuple(value, writer)
             else if (isArray(T))
                 try stringifyArray(value.items, writer)
@@ -58,16 +61,16 @@ pub const json = struct {
                 try stringifyHashMap(value, writer)
             else
                 try stringifyStruct(value, writer),
-            .Null => try writer.writeAll("null"),
-            .Optional => if (value) |unwrapped| try stringifyValue(unwrapped, writer) else try writer.writeAll("null"),
-            .Enum => |enum_info| if (enum_info.is_exhaustive) try writer.writeAll(@tagName(value)) else {
+            .null => try writer.writeAll("null"),
+            .optional => if (value) |unwrapped| try stringifyValue(unwrapped, writer) else try writer.writeAll("null"),
+            .@"enum" => |enum_info| if (enum_info.is_exhaustive) try writer.writeAll(@tagName(value)) else {
                 if (std.enums.tagName(T, value)) |tag|
                     try writer.writeAll(tag)
                 else
                     try writer.print("{d}", .{@as(enum_info.tag_type, @intFromEnum(value))});
             },
-            .EnumLiteral => try writer.writeAll(@tagName(value)),
-            .Union => try stringifyUnion(value, writer),
+            .enum_literal => try writer.writeAll(@tagName(value)),
+            .@"union" => try stringifyUnion(value, writer),
             else => @compileError("Cannot stringify type '" ++ @typeName(T) ++ "'"),
         }
     }
@@ -75,7 +78,7 @@ pub const json = struct {
     fn stringifyInt(value: anytype, writer: std.io.AnyWriter) !void {
         const char_map = "0123456789";
         const T = @TypeOf(value);
-        const int_info = @typeInfo(T).Int;
+        const int_info = @typeInfo(T).int;
         var abs_value = if (int_info.signedness == .unsigned) value else @abs(value);
         const digit_count = comptime blk: {
             break :blk std.math.log10(std.math.maxInt(T)) +
@@ -113,7 +116,7 @@ pub const json = struct {
 
     fn stringifyUnion(value: anytype, writer: std.io.AnyWriter) !void {
         const T = @TypeOf(value);
-        const union_info = @typeInfo(T).Union;
+        const union_info = @typeInfo(T).@"union";
         if (union_info.tag_type) |UnionTagType| {
             const fields: []const std.builtin.Type.UnionField = std.meta.fields(T);
             inline for (fields) |field| {
@@ -161,7 +164,7 @@ pub const json = struct {
 
     fn stringifyTuple(value: anytype, writer: std.io.AnyWriter) !void {
         const T = @TypeOf(value);
-        const struct_info = @typeInfo(T).Struct;
+        const struct_info = @typeInfo(T).@"struct";
         if (!struct_info.is_tuple) @compileError("Expected a tuple but got '" ++ @typeName(T) ++ "'");
         const fields: []const std.builtin.Type.StructField = std.meta.fields(T);
         inline for (fields, 0..) |field, i| {
@@ -211,9 +214,9 @@ pub const json = struct {
     inline fn isArray(T: type) bool {
         comptime {
             return switch (@typeInfo(T)) {
-                .Array => |array_type_info| array_type_info.child != u8,
-                .Pointer => |ptr_type_info| ptr_type_info.child != u8,
-                .Struct => @hasField(T, "items") and
+                .array => |array_type_info| array_type_info.child != u8,
+                .pointer => |ptr_type_info| ptr_type_info.child != u8,
+                .@"struct" => @hasField(T, "items") and
                     (T == std.ArrayList(std.meta.Child(std.meta.FieldType(T, .items))) or
                     T == std.ArrayListUnmanaged(std.meta.Child(std.meta.FieldType(T, .items)))),
                 else => false,
@@ -223,7 +226,7 @@ pub const json = struct {
 
     inline fn isHashMap(comptime T: type) bool {
         comptime {
-            return std.meta.activeTag(@typeInfo(T)) == .Struct and
+            return std.meta.activeTag(@typeInfo(T)) == .@"struct" and
                 @hasDecl(T, "iterator") and
                 (T == std.StringHashMapUnmanaged(getHashMapValueType(T)) or
                 T == std.AutoHashMapUnmanaged(getHashMapKeyType(T), getHashMapValueType(T)));
@@ -233,36 +236,36 @@ pub const json = struct {
     inline fn hasToString(comptime T: type) bool {
         comptime {
             return switch (@typeInfo(T)) {
-                .Struct => @hasDecl(T, "toString"),
+                .@"struct" => @hasDecl(T, "toString"),
                 else => false,
             };
         }
     }
 
     inline fn getHashMapKeyType(comptime T: type) type {
-        return @typeInfo(@typeInfo(@TypeOf(T.getKey)).Fn.return_type.?).Optional.child;
+        return @typeInfo(@typeInfo(@TypeOf(T.getKey)).@"fn".return_type.?).optional.child;
     }
 
     inline fn getHashMapValueType(comptime T: type) type {
-        return @typeInfo(@typeInfo(@TypeOf(T.get)).Fn.return_type.?).Optional.child;
+        return @typeInfo(@typeInfo(@TypeOf(T.get)).@"fn".return_type.?).optional.child;
     }
 
     inline fn isString(T: type, val: T) bool {
         return switch (@typeInfo(T)) {
-            .Array => |array_info| array_info.child == u8,
-            .Pointer => |ptr_info| switch (ptr_info.size) {
+            .array => |array_info| array_info.child == u8,
+            .pointer => |ptr_info| switch (ptr_info.size) {
                 .One => switch (@typeInfo(ptr_info.child)) {
-                    .Array => |array_info| array_info.child == u8,
+                    .array => |array_info| array_info.child == u8,
                     else => false,
                 },
                 .Many, .Slice => ptr_info.child == u8,
                 else => false,
             },
-            .Enum => |enum_info| if (enum_info.is_exhaustive)
+            .@"enum" => |enum_info| if (enum_info.is_exhaustive)
                 true
             else
                 std.enums.tagName(T, val) != null,
-            .EnumLiteral => true,
+            .enum_literal => true,
             else => false,
         };
     }
@@ -273,7 +276,166 @@ pub const json = struct {
         try stringify(value, buf.writer().any());
         return buf.toOwnedSlice();
     }
+
+    const Parser = struct {
+        const buf_len: usize = 256;
+
+        buf: [buf_len]u8 = .{0} ** buf_len,
+        slice: []u8 = &.{},
+        reader: std.io.AnyReader,
+
+        fn init(reader: std.io.AnyReader) Parser {
+            return .{ .reader = reader };
+        }
+
+        fn feed(parser: *Parser) !void {
+            if (parser.slice.len == 0) {
+                const bytes_read = try parser.reader.read(&parser.buf);
+                parser.slice = (&parser.buf)[0..bytes_read];
+            }
+        }
+
+        fn peekByte(parser: *Parser) !?u8 {
+            try parser.feed();
+            return parser.slice[0];
+        }
+
+        fn peekBytesAtLeast(parser: *Parser, comptime len: usize) !?[]const u8 {
+            if (len > buf_len) @compileError("peekBytes len has to be less than 256");
+            try parser.feed();
+            return if (len < parser.slice.len) parser.slice[0..len] else parser.slice;
+        }
+
+        fn consumeByte(parser: *Parser) !?u8 {
+            try parser.feed();
+            defer parser.slice = parser.slice[1..];
+            return parser.slice[0];
+        }
+
+        fn consumeWhitespace(parser: *Parser) !void {
+            try parser.feed();
+            while (try parser.peekByte()) |byte| {
+                switch (byte) {
+                    ' ', '\n', '\r', '\t' => _ = try parser.consumeByte(),
+                    else => return,
+                }
+            }
+        }
+
+        fn checkSlice(parser: *Parser, comptime slice: []const u8) !bool {
+            return std.mem.eql(u8, slice, try parser.peekBytesAtLeast(slice.len) orelse "");
+        }
+    };
+
+    pub fn parse(comptime T: type, reader: std.io.AnyReader) !T {
+        var parser: Parser = .init(reader);
+        try parser.consumeWhitespace();
+        return try parseType(T, &parser);
+    }
+
+    fn parseType(comptime T: type, parser: *Parser) !T {
+        switch (@typeInfo(T)) {
+            .bool => return try parseBool(parser),
+            .int => @compileError("int not implemented"),
+            .comptime_int => @compileError("comptime int not implemented"),
+            .float => @compileError("float not implemented"),
+            .comptime_float => @compileError("comtime float not implemented"),
+            .pointer => |ptr_info| switch (ptr_info.size) {
+                .One => switch (@typeInfo(ptr_info.child)) {
+                    .array => |array_info| if (array_info.child == u8)
+                        @compileError("string not implemented")
+                    else
+                        @compileError("array not implemented"),
+                    else => @compileError("value not implemented"),
+                },
+                .Many, .Slice => {
+                    // if (ptr_info.size == .Many and ptr_info.sentinel == null)
+                    //     @compileError("Cannot stringify type '" ++ @typeName(T) ++ "' without sentinel");
+                    // const slice = if (ptr_info.size == .Many) std.mem.span(value) else value;
+                    if (ptr_info.child == u8)
+                        @compileError("string not implemented")
+                    else
+                        @compileError("array not implemented");
+                },
+                else => @compileError("Cannot stringify type '" ++ @typeName(T) ++ "'"),
+            },
+            .array => |array_info| if (array_info.child == u8)
+                @compileError("string not implemented")
+            else
+                @compileError("array not implemented"),
+            .@"struct" => |struct_info| if (struct_info.is_tuple)
+                @compileError("tuple not implemented")
+            else if (isArray(T))
+                @compileError("array list not implemented")
+            else if (isHashMap(T))
+                @compileError("hashmap not implemented")
+            else
+                @compileError("struct not implemented"),
+            .null => @compileError("null not implemented"),
+            .optional => |optional_info| return try parseOptional(optional_info.child, parser),
+            .@"enum" => |enum_info| if (enum_info.is_exhaustive) {
+                @compileError("exhaustivve enum not implemented");
+            } else {
+                @compileError("non-exhaustive enum not implemented");
+            },
+            .enum_literal => @compileError("enum literal not implemented"),
+            .@"union" => @compileError("union not implemented"),
+            else => @compileError("Cannot parse type '" ++ @typeName(T) ++ "'"),
+        }
+    }
+
+    fn parseBool(parser: *Parser) !bool {
+        if (try parser.peekByte()) |byte|
+            switch (byte) {
+                't' => return if (try parser.checkSlice("true")) true else error.invalid_json,
+                'f' => return if (try parser.checkSlice("false")) false else error.invalid_json,
+                else => return error.invalid_json,
+            }
+        else
+            return error.invalid_json;
+    }
+
+    fn parseOptional(comptime T: type, parser: *Parser) !?T {
+        if (try parser.peekByte()) |byte|
+            switch (byte) {
+                'n' => return if (try parser.checkSlice("null")) null else error.invalid_json,
+                else => return try parseType(T, parser),
+            }
+        else
+            return error.invalid_json;
+    }
+
+    // TODO: parseIntoExisting - this one might not require all fields, but let's not do this until there's actually a need
+
+    // parse functions should require value, there should be optional parse function that accepts nulls
+    // so if struct is not optional, then the parser treats null as invalid_json
+    // fn parseValueBool(
+    //     parser: *Parser,
+    // ) !bool {}
+
+    pub fn parseFromSlice(comptime T: type, slice: []const u8) !T {
+        var stream = std.io.fixedBufferStream(slice);
+        return try parse(T, stream.reader().any());
+    }
 };
+
+test "parse" {
+    try std.testing.expectEqual(true, try json.parseFromSlice(bool,
+        \\            
+        \\  true 
+    ));
+    try std.testing.expectEqual(true, try json.parseFromSlice(bool, "true "));
+    try std.testing.expectError(error.invalid_json, json.parseFromSlice(bool, "tru"));
+    try std.testing.expectEqual(false, try json.parseFromSlice(bool,
+        \\            
+        \\  false 
+    ));
+    try std.testing.expectEqual(false, try json.parseFromSlice(bool, "false "));
+    try std.testing.expectError(error.invalid_json, json.parseFromSlice(bool, "fals"));
+    try std.testing.expectEqual(true, try json.parseFromSlice(?bool, "true "));
+    try std.testing.expectEqual(null, try json.parseFromSlice(?bool, "null"));
+    try std.testing.expectError(error.invalid_json, json.parseFromSlice(?bool, "nil"));
+}
 
 test "stringify struct" {
     const Bar = struct {
